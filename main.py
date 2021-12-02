005d6fb95471269d38f88dc8ebf18cd5e5fd7904
david <bullatsev@gmail.com>
2021-12-02 18:04:23 +0300

    add value2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def print_hi(name):
     print(f'My first git-repo{name}')
 
-def foo():
-    print('empty func')
+def foo(name):
+    print('empty func', name)
 
 if __name__ == '__main__':
     print_hi('name')
